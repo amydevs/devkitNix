793a1ef9d1ae5a371cdcef9a60291e5c5662aaaa
Samuel Åkesson <samak519@student.liu.se>
2026-03-19 19:52:30 +0100

    add wii-u example

---
+++ examples/wii-u/flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    devkitNix.url = "github:bandithedoge/devkitNix";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      devkitNix,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ devkitNix.overlays.default ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell.override { stdenv = pkgs.devkitNix.stdenvPPC; } { };
+        packages.default = pkgs.devkitNix.stdenvPPC.mkDerivation {
+          name = "wut-example";
+          src = ./.;
+
+          makeFlags = [ "TARGET=example" ];
+          installPhase = ''
+            mkdir $out
+            cp example.wuhb $out
+          '';
+        };
+      }
+    );
+}
